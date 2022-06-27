1ecd87434517f6e75f1cde3b8a76f1f62fa615b2
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-27 10:14:05 +0300

    initialised all variables

---
+++ js/challenge.js
@@ -0,0 +1,5 @@
+//initialised all variables 
+let c = document.querySelector("#counter"), p=document.getElementById("pause"), active = true, dec = document.getElementById("minus"),add = document.getElementById("plus"), heart = document.getElementById("heart"), like = document.querySelector(".likes"), clicked = [];
+let f = document.querySelector("form"), av = document.querySelector("#comment-input");
+let d = document.querySelector(".comments");
+let cr = 0, cl = 1;
